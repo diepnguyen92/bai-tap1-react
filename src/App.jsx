26dce56e086f86cc8a9159b05d2efdaf48c1cd59
diepnguyen92 <148781455+diepnguyen92@users.noreply.github.com>
2024-04-18 09:35:24 +0700

    up

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navigation from './component/Navigationt/Navigation'
+import Header from './component/Header/Header'
+import Section from './component/section/Section'
+import Footer from './component/Footer/Footer'
+
+
+function App() {
+  
+
+  return (
+    <>
+   <Navigation/>
+   <Header />
+   <Section />
+   <Footer />
+
+    </>
+  )
+}
+
+export default App
